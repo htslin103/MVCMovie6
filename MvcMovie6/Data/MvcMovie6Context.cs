b0901d2f58e6cf849fcfa0bf6d0b7f2c21dee2d3
Helen L <htslin103@gmail.com>
2023-03-26 16:32:33 -0600

    Initial commit

---
+++ MvcMovie6/Data/MvcMovie6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie6.Models;
+
+namespace MvcMovie6.Data
+{
+    public class MvcMovie6Context : DbContext
+    {
+        public MvcMovie6Context (DbContextOptions<MvcMovie6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie6.Models.Movie> Movie { get; set; } = default!;
+    }
+}
